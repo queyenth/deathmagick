e391775b2e7d5dd4826b5485da0cf241101b66a7
Andrew Zhupin <overan16@gmail.com>
2013-06-23 00:03:36 +0700

    Added simple TwoIceMeteor, need improvement

--- MeteorFactory.h
+++ MeteorFactory.h
@@ -315,6 +315,57 @@ public:
   int movedDistance;
 };
 
+class TwoIceMeteor : public Meteor {
+  std::shared_ptr<Meteor> base;
+public:
+  TwoIceMeteor(Meteor *meteor) : base(meteor) {range = 200;countOfFrames = 0;falling = true;}
+
+  virtual void Cast(Player &player) override {
+    base->Cast(player);
+  }
+
+  virtual bool operation() override {
+    if (base->operation())
+      return true;
+    if (falling) {
+      window.Draw(base.get());
+      int left = base->GetX() - base->GetRange();
+      int right = base->GetX() + base->GetWidth() + base->GetRange();
+      for (auto it = enemies.begin(); it != enemies.end(); it++) {
+        if (left <= it->GetX() && it->GetX() + it->GetWidth() <= right) {
+          it->Freeze(2000);
+        }
+      }
+      falling = false;
+    }
+    if (!iceFromLandImage.IsValid()) {
+      iceFromLandImage.LoadFromFile("img\\ice_from_lang.png");
+      iceFromLand.SetImage(iceFromLandImage);
+      iceFromLand.SetY(base->GetY() - 42);
+    }
+    iceFromLand.Move(0, 21);
+    int left = base->GetX() - GetRange();
+    int right = base->GetX() + base->GetWidth() + GetRange();
+    if (countOfFrames == 2) {
+      for (auto it = enemies.begin(); it != enemies.end(); it++)
+        if (left <= it->GetX() && it->GetX() + it->GetWidth() <= right)
+          it->DamageHim(25);
+      return false;
+    }
+    for (int i = left; i < right; i+=21) {
+      iceFromLand.SetX(i);
+      window.Draw(&iceFromLand);
+    }
+    countOfFrames++;
+    return true;
+  }
+
+  se::Sprite iceFromLand;
+  se::Image iceFromLandImage;
+  int countOfFrames;
+  bool falling;
+};
+
 class MeteorFactory {
 public:
   MeteorFactory() {}
@@ -334,6 +385,8 @@ public:
       return std::shared_ptr<Meteor>(new OneIceAndOneLightMeteor(new BaseMeteor()));
     else if (impr.k1 == 1 && impr.k2 == 1 && impr.k3 == 1)
       return std::shared_ptr<Meteor>(new OneFireAndOneIceAndOneLightMeteor(new BaseMeteor()));
+    else if (impr.k1 == 0 && impr.k2 == 2 && impr.k3 == 0)
+      return std::shared_ptr<Meteor>(new TwoIceMeteor(new BaseMeteor()));
     else
       return std::shared_ptr<Meteor>(new BaseMeteor());
   }
